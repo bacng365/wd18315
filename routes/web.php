7589dcaceb8a9f711ef35e11d7fcb38b1d533330
bacnguyen <bacng365@gmail.com>
2024-07-16 14:57:30 +0700

    class 1307

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,6 @@ use Illuminate\Support\Facades\Route;
 // Route: http://127.0.0.1:8000/test
 // Route được viết theo chuẩn sale(Ngăn cách các từ bằng "-", không dấu, viết thường)
 // VD: http://127.0.0.1:8000/san-pham
-Route::get('test', function() {
-    echo "123";
-});
 
 // Route: http://127.0.0.1:8000
 Route::get('/', function() {
@@ -76,4 +73,10 @@ Route::group(['prefix' => 'products', 'as' => 'products.'], function() {
     // Route::get('delete-product/{idProduct}', [Lab2ProductController::class, 'deleteProducts'])->name('deleteProducts');
     Route::delete('delete-product', [Lab2ProductController::class, 'deleteProducts'])->name('deleteProducts');
     Route::get('search-product', [Lab2ProductController::class, 'searchProduct'])->name('searchProduct');
+});
+
+
+
+Route::get('test', function() {
+    return view('admin.products.list-product');
 });
